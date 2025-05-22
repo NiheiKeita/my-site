7330733ec71bd0ce999d24618318a22de2cec2bb
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-05-22 22:12:48 +0900

    fix next.config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextPWA = require('next-pwa')({
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/my-site' : '',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextPWA(nextConfig)
